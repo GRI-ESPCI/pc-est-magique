90649bad65674adca2817b872209aa5686ceda8d
louisld <louisldthesamui@gmail.com>
2023-12-15 01:05:24 +0100

    V4A edition backend

--- app/routes/v4a/routes.py
+++ app/routes/v4a/routes.py
@@ -1,11 +1,14 @@
 """PC est magique - V4A Pages Routes"""
 
+import os
+
 import flask
+from flask import jsonify
 from flask_babel import _
 
 from app.routes.v4a import bp
-from app.utils import typing
-
+from app import helpers, db
+from app.utils import typing, wysiwyg
 from app.models import V4A
 from app.routes.v4a import forms
 
@@ -41,7 +44,22 @@ def edit(v4a_id: int) -> typing.RouteReturn:
     edit_form = forms.EditV4A(obj=v4a)
 
     if edit_form.validate_on_submit():
-        pass
+        edit_form.populate_obj(v4a)
+        v4a.full_description = wysiwyg.parse_delta_to_html(
+            edit_form.delta_full_description.data
+        )
+
+        if edit_form.image_file.data:
+            with open(os.path.join("app", "static", "img", "v4a",
+            f"{v4a.id}.png"), "wb") as fh:
+                fh.write(edit_form.image_file.data.read())
+
+        db.session.commit()
+
+        flask.flash(_("V4A mis à jour avec succès."), "success")
+
+        return helpers.ensure_safe_redirect("v4a.full_description", v4a_id=v4a.id)
+
 
     return flask.render_template(
         "v4a/edit.html",
