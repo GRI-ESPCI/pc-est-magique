ec3e0b2648ca46ff07bb4588a6ad480cb3372f43
Alitero <alitero.k@gmail.com>
2026-08-07 02:06:21 +0200

    More calendar fixes
    Automatically scroll to earliest event time on week view
    
    Fixed an issue where all-day event that lasted more than one day were exported as several one day events

--- app/routes/calendar/routes.py
+++ app/routes/calendar/routes.py
@@ -324,9 +324,9 @@ def export_feed(token: str):
         tz = zoneinfo.ZoneInfo("Europe/Paris")
         
         if event.all_day:
+            e.make_all_day()
             e.begin = event.start_time.date()
             e.end = event.end_time.date() + datetime.timedelta(days=1)
-            e.make_all_day()
         else:
             e.begin = event.start_time.replace(tzinfo=tz)
             e.end = event.end_time.replace(tzinfo=tz)
